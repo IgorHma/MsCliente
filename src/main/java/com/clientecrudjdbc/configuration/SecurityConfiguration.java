aeae5feeb619419d08dfcf3e9ce5dbc4e9e3c1ea
IgorHma <55795539+IgorHma@users.noreply.github.com>
2024-12-19 21:41:42 -0300

    igor_hma
    ajuste na SecurityConfiguration

--- src/main/java/com/clientecrudjdbc/configuration/SecurityConfiguration.java
+++ src/main/java/com/clientecrudjdbc/configuration/SecurityConfiguration.java
@@ -14,18 +14,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfiguration {
 
-    /*@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/public/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-        return http.build();
-    }*/
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -42,9 +30,7 @@ public class SecurityConfiguration {
                 .httpBasic();
         return http.build();
     }
-
-
-
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
